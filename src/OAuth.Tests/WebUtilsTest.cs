1a23c32f5a269ec2503c1a7ca09f33c7c0228789
Martin Novák <m.novak@gmc.net>
2018-01-20 20:53:11 +0100

    Bug fix: added request url query string parameters into signature base.

---
+++ src/OAuth.Tests/WebUtilsTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace OAuth.Tests
+{
+    [TestFixture]
+    public class WebUtilsTest
+    {
+        [TestCase("https://www.google.com", 0)]
+        [TestCase("https://www.google.com/", 0)]
+        [TestCase("https://www.google.com/?a=b", 1)]
+        [TestCase("https://www.google.com/?a=b&c=d", 2)]
+        [TestCase("https://www.google.com/?a=b&c=d&e=f", 3)]
+        public void ParsesQueryString(string url, int expectedQueryParametersCount)
+        {
+            Assert.AreEqual(expectedQueryParametersCount, WebUtils.ParseQueryString(new Uri(url)).Count());
+        }
+
+        [TestCase("https://www.google.com/?")]
+        [TestCase("https://www.google.com/?a=b&c=d&e=f&")]
+        public void ParseQueryStringThrowsForInvalidQueryString(string invalidUrl)
+        {
+            // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+            Assert.Throws<ArgumentException>(() => WebUtils.ParseQueryString(new Uri(invalidUrl)).ToList());
+        }
+    }
+}
