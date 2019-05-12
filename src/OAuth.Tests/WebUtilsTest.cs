50e1f6fa7840b0bf8874963336118bb2cb337986
Anton Kalcik <7405311+akalcik@users.noreply.github.com>
2019-05-12 11:14:58 +0200

    Remove checking of scheme and disallow null query arguments.
    For more details see comments in #8.

--- src/OAuth.Tests/WebUtilsTest.cs
+++ src/OAuth.Tests/WebUtilsTest.cs
@@ -12,20 +12,19 @@ namespace OAuth.Tests
         [TestCase("https://www.google.com/?a=b", 1)]
         [TestCase("https://www.google.com/?a=b&c=d", 2)]
         [TestCase("https://www.google.com/?a=b&c=d&e=f", 3)]
-        [TestCase("https://www.google.com/?a=b&c=d&e=f&",4)]
-        [TestCase("https://www.google.com/?a=b&c=d&e=f&g=",4)]
+        [TestCase("https://www.google.com/?a=b&c=d&e=f&g=", 4)]
         [TestCase("https://www.url.com/search?jql=assignee=User&user_id=UserId", 2)]
         public void ParsesQueryString(string url, int expectedQueryParametersCount)
         {
             Assert.AreEqual(expectedQueryParametersCount, WebUtils.ParseQueryString(new Uri(url)).Count());
         }
 
-        [TestCase("https://www.url..com")]
         [TestCase("https://www.google.com/?")]
+        [TestCase("https://www.google.com/?a=b&c=d&e=f&")]
         public void ParseQueryStringThrowsForInvalidQueryString(string invalidUrl)
         {
             // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
-            Assert.Throws<UriFormatException>(() => WebUtils.ParseQueryString(new Uri(invalidUrl)).ToList());
+            Assert.Throws<ArgumentException>(() => WebUtils.ParseQueryString(new Uri(invalidUrl)).ToList());
         }
 
         [TestCase(null)]
--- src/OAuth/WebUtils.cs
+++ src/OAuth/WebUtils.cs
@@ -12,23 +12,24 @@ namespace OAuth
         {
             if (uri is null) { throw new ArgumentNullException(nameof(uri)); }
 
-            var isUriValidHttpScheme = false;
-            if (Uri.TryCreate(uri.ToString(), UriKind.Absolute, out Uri validatedUri))
-            {
-                isUriValidHttpScheme = (validatedUri.Scheme == Uri.UriSchemeHttp || validatedUri.Scheme == Uri.UriSchemeHttps);
-            }
-
-            if(!isUriValidHttpScheme) { throw new UriFormatException( "Uri is not valid Url");}
-
             NameValueCollection parsedQuery = HttpUtility.ParseQueryString(uri.Query);
 
+            var uriDoesNotHaveValidQueryStringExceptionMessage = "Uri does not have valid query string";
             if (uri.Query.Any() && parsedQuery.Count == 0)
             {
-                throw new UriFormatException("Uri does not have valid query string");
+                throw new ArgumentException(uriDoesNotHaveValidQueryStringExceptionMessage);
             }
 
             var queryStringParameters =
                 parsedQuery.AllKeys.SelectMany(parsedQuery.GetValues, (key, value) => new {key, value});
+
+            // ReSharper disable once PossibleMultipleEnumeration
+            if (queryStringParameters.Any(queryStringParameter => queryStringParameter.key == null))
+            {
+                throw new ArgumentException(uriDoesNotHaveValidQueryStringExceptionMessage);
+            }
+
+            // ReSharper disable once PossibleMultipleEnumeration
             foreach (var param in queryStringParameters)
             {
                 yield return new WebParameter(param.key, param.value);
